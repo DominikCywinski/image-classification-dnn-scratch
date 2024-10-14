80572c33b084976eeac76f97d057dbc73b36968c
DominikCywinski <dominik.cywinski988@gmail.com>
2024-10-14 17:32:54 +0200

    Add predict  and train scripts.

---
+++ predict.py
@@ -0,0 +1,26 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import re
+
+from image_classification_utils import predict, load_model, load_data, convert_image
+
+
+if __name__ == "__main__":
+    folder_path = "predict_data"
+    model_path = "./models/model_64_64_3.pkl"
+
+    _, _, _, _, classes = load_data("datasets/train_catvnoncat.h5", "datasets/test_catvnoncat.h5")
+    parameters = load_model(model_path)
+    org_size = [int(num) for num in re.findall(r"\d+", model_path)]
+
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith(".png"):
+            png_path = os.path.join(folder_path, file_name)
+            image, converted_image = convert_image(png_path, org_size)
+
+            my_predicted_image = predict(converted_image, 1, parameters)
+
+            plt.imshow(image)
+            plt.title(classes[int(np.squeeze(my_predicted_image)),].decode("utf-8"))
+            plt.show()
---
+++ train.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from image_classification_utils import load_data, L_layer_model, save_model
+
+
+if __name__ == "__main__":
+    train_x_orig, train_y, test_x_orig, test_y, classes = load_data(
+        "datasets/train_catvnoncat.h5", "datasets/test_catvnoncat.h5"
+    )
+
+    train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T
+    test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+    train_x = train_x_flatten / 255
+    test_x = test_x_flatten / 255
+
+    nn_input_size = np.prod(train_x_orig.shape[1:])
+    layers_dims = [nn_input_size, 20, 7, 5, 1]  #  4-layer model
+
+    parameters = L_layer_model(train_x, train_y, layers_dims, learning_rate=0.01, num_iterations=3000, print_cost=True)
+
+    save_model(
+        parameters,
+        file_path=f"./models/model_{train_x_orig.shape[1]}_{train_x_orig.shape[2]}_{train_x_orig.shape[3]}.pkl",
+    )
